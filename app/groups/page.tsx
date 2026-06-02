f65027825722fd397f3c563701ca66047d97ecfb
mrozsansky-blip <mrozsansky@tiferes.net>
2026-06-02 16:37:33 -0400

    Add groups page

---
+++ app/groups/page.tsx
@@ -0,0 +1,9 @@
+import { GroupsTable } from '@/components/GroupsTable';
+
+export default function GroupsPage() {
+  return (
+    <main className="min-h-screen p-8">
+      <GroupsTable />
+    </main>
+  );
+}
