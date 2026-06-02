9d5f3100c49164b4d78caf4e8128afea3c8742b4
mrozsansky-blip <mrozsansky@tiferes.net>
2026-06-02 16:37:21 -0400

    Add inbox page

---
+++ app/inbox/page.tsx
@@ -0,0 +1,9 @@
+import { ConversationShell } from '@/components/ConversationShell';
+
+export default function InboxPage() {
+  return (
+    <main className="min-h-screen p-4">
+      <ConversationShell />
+    </main>
+  );
+}
