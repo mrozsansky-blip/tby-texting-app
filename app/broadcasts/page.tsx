1c5ad51414d43aa98165a7836e8fbcf0afaafd42
mrozsansky-blip <mrozsansky@tiferes.net>
2026-06-04 12:17:08 -0400

    Add broadcasts visual workflow page

---
+++ app/broadcasts/page.tsx
@@ -0,0 +1,9 @@
+import { BroadcastsShell } from '@/components/BroadcastsShell';
+
+export default function BroadcastsPage() {
+  return (
+    <main className="min-h-screen p-4">
+      <BroadcastsShell />
+    </main>
+  );
+}
